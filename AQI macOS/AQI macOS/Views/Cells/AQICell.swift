f5629a0d8bbaaa583cdce66110deb8bdd8b2e668
Luis Fernando Mata Licón <fernando@fernandomata.mx>
2019-04-23 18:09:51 +0800

    Add minimum size according to AQI Cell. Add AQI Cell class.

---
+++ AQI macOS/AQI macOS/Views/Cells/AQICell.swift
@@ -0,0 +1,19 @@
+//
+//  AQICell.swift
+//  AQI macOS
+//
+//  Created by Fernando Mata on 4/23/19.
+//  Copyright © 2019 Fernando Mata. All rights reserved.
+//
+
+import Cocoa
+
+class AQICell: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
---
+++ AQI macOS/AQI macOS/Views/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  AQI macOS
+//
+//  Created by Fernando Mata on 4/23/19.
+//  Copyright © 2019 Fernando Mata. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
