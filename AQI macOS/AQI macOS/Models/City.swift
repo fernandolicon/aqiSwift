3c79367e26f3e1419d7c4ea800a633e1c9de9b78
Luis Fernando Mata Licón <fernando@fernandomata.mx>
2019-04-30 11:39:20 +0800

    Implement City inits for Marshal and Realm.

--- AQI macOS/AQI macOS/Models/City.swift
+++ AQI macOS/AQI macOS/Models/City.swift
@@ -7,19 +7,39 @@
 //
 
 import Foundation
+import Realm
 import RealmSwift
 import CoreLocation
+import Marshal
 
-class City: Object {
+class City: Object, Unmarshaling {
     var name: String = ""
-    var urlString: String?
-    var lat: CLLocationDegrees?
-    var long: CLLocationDegrees?
+    var urlString: String = ""
+    var lat: Double = 0.0
+    var long: Double = 0.0
     var order: Int = 0
     
     var url: URL? {
-        return URL(string: urlString ?? "")
+        return URL(string: urlString)
+    }
+    
+    required init(object: MarshaledObject) throws {
+        super.init()
+        
+        name = try object.value(for: "name")
+        urlString = try object.value(for: "url")
+    }
+    
+    required init() {
+        super.init()
+    }
+    
+    required init(realm: RLMRealm, schema: RLMObjectSchema) {
+        super.init(realm: realm, schema: schema)
+    }
+    
+    required init(value: Any, schema: RLMSchema) {
+        super.init(value: value, schema: schema)
     }
 }
-
 
